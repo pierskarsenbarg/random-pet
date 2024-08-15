54cbaa0d25ebbc6422d89bc337ecba6548fb3b68
Piers Karsenbarg <piers@pulumi.com>
2024-08-15 15:35:31 +0100

    updated pet name length

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
 const pet = new random.RandomPet("pet", {
-    length: 2
+    length: 3
 });
 
 export const petName = pet.id;
